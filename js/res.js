bbee672125d9cd06616555598a292745734bf387
shivamkpsingh <shivamkpsingh@gmail.com>
2021-05-11 08:26:50 +0530

    change something

--- js/res.js
+++ js/res.js
@@ -1,6 +1,23 @@
-burger=document.querySelector('.burger')
-navcontent=document.querySelector('.nav-containt')
-burger.addEventListener('click',()=>{
- navcontent.classList.toggle('v-class')
- navcontent.classList.toggle('h-nav')
+burger = document.querySelector('.burger')
+navcontent = document.querySelector('.nav-containt')
+burger.addEventListener('click', () => {
+    navcontent.classList.toggle('v-class')
+    navcontent.classList.toggle('h-nav')
 })
+
+
+
+
+
+var typed = new Typed(".typing", {
+    strings: ["Front-End Engineer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing_1", {
+    strings: ["Front-End Engineer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
